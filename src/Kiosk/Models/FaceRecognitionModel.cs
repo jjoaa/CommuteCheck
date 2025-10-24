f519fffcaa9998dc1c292773597741dc102e6321
jjoaa <59039168+jjoaa@users.noreply.github.com>
2025-10-24 13:48:30 +0900

    feat:[#4] Page UI에 위치명, 시간, 대기자 명단 반영
    - FaceRecognitionModel settings [25.08.06]

---
+++ src/Kiosk/Models/FaceRecognitionModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Kiosk.Models
+{
+    public class FaceRecognitionModel : INotifyPropertyChanged
+    {
+        private long _hostLocationOid;
+        public long HostLocationOid
+        {
+            get => _hostLocationOid;
+            set { _hostLocationOid = value; OnPropertyChanged(); }
+        }
+
+        private string _locationName;
+        public string LocationName
+        {
+            get => _locationName;
+            set { _locationName = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<WaitUser> _waitUsers = new();
+        public ObservableCollection<WaitUser> WaitUsers
+        {
+            get => _waitUsers;
+            set { _waitUsers = value; OnPropertyChanged(); }
+        }
+
+        private DateTime _currentDateTime = DateTime.Now;
+        public DateTime CurrentDateTime
+        {
+            get => _currentDateTime;
+            set { _currentDateTime = value; OnPropertyChanged(); }
+        }
+        // 상태문구 + 자동 복귀(초)
+        private string _statusText = "가까이/정면/조명 확인해주세요";
+        public string StatusText
+        {
+            get => _statusText;
+            set { _statusText = value; OnPropertyChanged(); }
+        }
+
+        public int StatusAutoRevertSec { get; set; } = 0;
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
