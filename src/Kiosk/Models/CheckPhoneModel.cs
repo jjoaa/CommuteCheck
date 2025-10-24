7b08519b58a33868179df2172ad044e989419c9b
jjoaa <59039168+jjoaa@users.noreply.github.com>
2025-10-24 13:55:00 +0900

    feat:[#8] CheckPhoneModel settings
    - feat:[#8] UserOid, HostLocationOid 타입 long으로 변경 [25.10.02]

---
+++ src/Kiosk/Models/CheckPhoneModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Kiosk.Models
+{
+    public class CheckPhoneModel
+    {
+        public bool Success { get; set; }
+        public string Result { get; set; }
+        public string FirebaseToken { get; set; }
+        public string Type { get; set; } // 0이면 유저한테 FCM보낸다. 1이면 서버에 저장한다.
+
+        // 대기자 명단 관련 추가 정보
+        public string UserName { get; set; }
+        public long UserOid { get; set; }
+        public long HostLocationOid { get; set; }
+        public string Landmark { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
