b36465d23ec01bc0fb75dbad8c0a2b012f4dda6e
jjoaa <59039168+jjoaa@users.noreply.github.com>
2025-10-27 16:08:38 +0900

    feat:[#2] LoginPage settings
    feat:[#2] Initial LoginPage [25.07.30]
    style:[#2] btn 세팅 수정 [25.08.07]
    style:[#2] 로그인페이지 UI 수정 [25.09.17]

---
+++ src/Kiosk/Pages/LoginPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Kiosk.Models;
+using Kiosk.Services.Interface;
+using Kiosk.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kiosk.Pages
+{
+    /// <summary>
+    /// LoginPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage(ViewModels.LoginViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext != null)
+            {
+                ((ViewModels.LoginViewModel)DataContext).Password = ((PasswordBox)sender).Password;
+            }
+        }
+    }
+}
