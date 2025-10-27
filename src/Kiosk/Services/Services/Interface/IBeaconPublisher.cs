b3e630c75bc83307a609d3782bd7cdf42650c8ae
jjoaa <59039168+jjoaa@users.noreply.github.com>
2025-10-27 16:48:16 +0900

    feat:[#3] Beacon settings
    feat: [#3] B-BLE advertise 확인 [25.08.04]

---
+++ src/Kiosk/Services/Services/Interface/IBeaconPublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk.Services.Interface
+{
+    public interface IBeaconPublisher
+    {
+        void StartIBeacon(string uuid, ushort major, ushort minor);
+        void StopIBeacon();
+    }
+}
