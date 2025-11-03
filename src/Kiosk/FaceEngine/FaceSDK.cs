3265338b6f073952356371da079a7b63a78a926c
jjoaa <59039168+jjoaa@users.noreply.github.com>
2025-11-03 16:15:25 +0900

    Update FaceSDK.cs

--- src/Kiosk/FaceEngine/FaceSDK.cs
+++ src/Kiosk/FaceEngine/FaceSDK.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
+namespace Kiosk.FaceEngine
+{
+    public class FaceSDK
+    {
+
+    }
+}
